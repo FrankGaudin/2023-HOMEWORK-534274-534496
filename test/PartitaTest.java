1b90376bb14530d6c95e79dfbbb21774186b67c9
Frank KH <72342418+FrankGaudin@users.noreply.github.com>
2023-03-26 16:43:14 +0200

    Added labirint class and fixet other class to implement it

---
+++ test/PartitaTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PartitaTest {
+
+
+	Partita p = new Partita();
+	Stanza s = new Stanza("Stanza");
+	
+	@Test
+	public void testGetStanzaVincente() {
+		assertEquals("Biblioteca", p.getLabirinto().getStanzaVincente().getNome());
+	}
+
+	@Test
+	public void testSetStanzaCorrente() {
+		p.getLabirinto().setStanzaCorrente(s);
+		assertEquals(s, p.getLabirinto().getStanzaCorrente());
+	}
+
+	@Test
+	public void testIsFinita() {
+		
+		assertFalse(p.isFinita());
+	}
+	
+}
---
+++ test/StanzaTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StanzaTest {
+	
+	Stanza s1 = new Stanza("s1");
+	Stanza s2= new Stanza("s2");
+	Attrezzo m = new Attrezzo("martello", 42);
+	
+	@Test
+	public void testGetStanzaAdiacente() {
+		assertNull(s1.getStanzaAdiacente("sud"));
+	}
+	
+
+	@Test
+	public void testImpostaStanzaAdiacente() {
+		s1.impostaStanzaAdiacente("sud", s2);
+		assertEquals(s2, s1.getStanzaAdiacente("sud"));
+	}
+	
+	@Test
+	public void testAddAttrezzo() {
+		
+		assertTrue(s1.addAttrezzo(m));
+	}
+	
+
+}
